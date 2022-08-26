fe0cdd3810492b50930bdc923040678a4c6e2578
Mehmet Tığlıoğlu <105611803+mehmettiglioglu@users.noreply.github.com>
2022-08-26 14:57:37 +0300

    updated

---
+++ README.md
@@ -0,0 +1,9 @@
+# Proje Hakkında 
+- HTML,CSS ve JavaScript kullanarak Memory Game yaptım.
+- Foreach, fonksiyonlar, array metotları, setTimeout,setInterval gibi yapıları kullandım.
+- Resimlerin yeri sayfa her yenilendiğinde değişiyor.
+- Seçilen kartlara CSS yardımıyla efekt verdim.
+- Yazdığım kodları yorum satırlarıyla açıkladım.
+
+# Proje Görseli
+![EkranGörüntüsü](/Screenshot_9.png)
--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+ // Selector seçimi 
 let images=document.querySelectorAll(".image");
-let timeArea=document.querySelector(".time-area")
+let timeArea=document.querySelector(".time-area");
+// Değişkenler 
 const imageURL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0BXoWfT_A10v895WuK1RrdoGjVf_xLGfUJg&usqp=CAU";
 let counter=0;
 let second=0;
 let minute=0;
 
+// Her seferinde fotoğrafların farklı yerlere gelmesini sağlayan fonksiyon.
 function shuffleCards()
 {
     let randomNumberArray=[];
@@ -32,54 +35,66 @@ function shuffleCards()
         }
 }
 
-
+// Oyun bittiğinde süreyi göstermek için yazdığım fonksiyon
 function timer()
 {
-setInterval(() => {
+ setInterval(() => {
     second++;
     if(second==60)
     {
         minute++;
     }
-    timeArea.innerHTML="Time:"+minute<10?"0"+minute:minute +":"+second<10?"0"+second:second
+    // timeArea.innerHTML="Time:"+minute<10?"0"+minute:minute +":"+second<10?"0"+second:second
 }, 1000);
 }
 
 
-
+// Her bir fotoğrafa click eventi vermek için foreach kullandım.
 images.forEach(element => {
-    element.addEventListener("click",(e)=>{
+element.addEventListener("click",(e)=>{
     
-
+    // Tıklanan resimleri bir değişkende tuttum.
         let selectedCard=e.target;
+    // Değişkene bir class ekledim kontrol edebilmek için.
         selectedCard.classList.add("flipped");
+    // Class eklediğim öğeleri seçtim.
         let flippedCard=document.querySelectorAll(".flipped");
 
-               
+      // Seçilen resimlerin linkini alt bölümünde verdiğim resimle değiştirdim.         
           flippedCard[0].src=flippedCard[0].alt;
           flippedCard[1].src=flippedCard[1].alt;
 
+         // Eğer iki tane kart seçilmişse
           if(flippedCard.length==2)
           {
+            // Seçilen kartların alt urlleri eşit mi diye kontrol ettim.
               if(flippedCard[0].alt==flippedCard[1].alt)
                 {
+                    // Eşitse aynı resimdir ve "matched" adında bir class ekledim.
                     flippedCard[0].classList.add("matched");
                     flippedCard[1].classList.add("matched");
-
+                    // Ve seçilen sınıfa eklediğim "flipped" sınıfını kaldırdım.
                     flippedCard[0].classList.remove("flipped");
                     flippedCard[1].classList.remove("flipped");
+                    // Başlangıçta 0 olarak tanımladığım counter her eşleşmede bir artacak. Oyunun bitmesini kontrol etmek için tanımladım.
                     counter++;
+                        //Eğer counter 5 olduysa oyun bitmiştir.
                         if(counter==5)
                         {
-                            timeArea.innerHTML=minute+":"+ second;
+                            // Oyunu ne kadar sürede bitirdiğini ekrana getiren kod.
+                            timeArea.innerHTML=`Your time was : ${second} second `
+                           
                         }
                       
                 }
                 else
                 {
+                    // Eğer seçilen 2 resimin alt urlleri birbirine eşleşmiyorsa
                     setTimeout(() => {
+                        // Başlangıç görsellerine geri döndürüyorum
                         flippedCard[0].src=imageURL;
                         flippedCard[1].src=imageURL;
+                        // Seçilen kartlardan "flipped" sınıfını çıkarıyorum.
                         flippedCard[0].classList.remove("flipped");
                         flippedCard[1].classList.remove("flipped");
                     }, 750);
